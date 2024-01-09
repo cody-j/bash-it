7aa1708014767333d242201140b112c129a9f934
Cody J <codywj777@gmail.com>
2024-01-08 21:26:25 -0800

    fixes simple theme and minimal scm output

--- themes/simple/simple.theme.bash
+++ themes/simple/simple.theme.bash
@@ -5,7 +5,7 @@
 #added TITLEBAR for updating the tab and window titles with the pwd
 case $TERM in
 	xterm*)
-	TITLEBAR="\[\033]0;\w\007\]"
+	TITLEBAR="\[\033]0;\j\007\]"
 	;;
 	*)
 	TITLEBAR=""
@@ -13,13 +13,13 @@ case $TERM in
 esac
 
 function prompt_command() {
-	PS1="${TITLEBAR}${orange}${reset_color}${green}\w${bold_blue}\[$(scm_prompt_info)\]${normal} "
+	PS1="${TITLEBAR}${orange}${reset_color}${bold_yellow}${bold_yellow}\W ${bold_purple}\[$(scm_prompt_info)\]\n${normal}>${normal} "
 }
 
 # scm themeing
-SCM_THEME_PROMPT_DIRTY=" ✗"
-SCM_THEME_PROMPT_CLEAN=" ✓"
-SCM_THEME_PROMPT_PREFIX="("
-SCM_THEME_PROMPT_SUFFIX=")"
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=""
+SCM_THEME_PROMPT_PREFIX=""
+SCM_THEME_PROMPT_SUFFIX=""
 
 safe_append_prompt_command prompt_command
